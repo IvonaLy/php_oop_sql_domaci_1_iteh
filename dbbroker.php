4f06ce08a267530cf738830577ca639859f4f3f0
IvonaLy <ivonavlasic99@gmail.com>
2022-08-28 09:07:34 +0200

    dodate forme za login i registraciju

---
+++ dbbroker.php
@@ -0,0 +1,12 @@
+<?php
+ 
+     
+        $hostname = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "beauty_salon";
+        
+        $conn = mysqli_connect($hostname,$username,$password,$database) or die("Database connection failed");
+     
+
+?>
--- register.php
+++ register.php
@@ -38,9 +38,23 @@
                     backdrop-filter: blur(30px);
                     ">
                 <div class="card-body p-5 shadow-5 text-center">
-                    <h2 class="fw-bold mb-5">Login</h2>
+                    <h2 class="fw-bold mb-5">Sign up now</h2>
                     <form>
-                  
+                    <!-- 2 column grid layout with text inputs for the first and last names -->
+                    <div class="row">
+                        <div class="col-md-6 mb-4">
+                        <div class="form-outline">
+                            <input type="text" id="form3Example1" class="form-control" />
+                            <label class="form-label" for="form3Example1">First name</label>
+                        </div>
+                        </div>
+                        <div class="col-md-6 mb-4">
+                        <div class="form-outline">
+                            <input type="text" id="form3Example2" class="form-control" />
+                            <label class="form-label" for="form3Example2">Last name</label>
+                        </div>
+                        </div>
+                    </div>
 
                     <!-- Email input -->
                     <div class="form-outline mb-4">
@@ -57,13 +71,12 @@
 
                     <!-- Submit button -->
                     <button type="submit" class="btn btn-primary btn-block mb-4">
-                       Log in
+                        Sign up
                     </button>
 
                  
                     </form>
                 </div>
-                <p>You dont have an account? <a href="register.php">Register now!</a></p>
                 </div>
             </div>
 
